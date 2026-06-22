ca8c291abf86b3ba5bd19e7bb605dca141be9c6e
briangitfree <vanme98@yahoo.com>
2026-06-22 12:05:19 -0700

    Larger and smaller number

---
+++ 01-larger-or-smaller.js
@@ -0,0 +1,18 @@
+// This program is recognized the larger of two integers entered by the user
+// Comp 648 JavaScript 1
+// Author: Brian Dinh
+
+
+
+// Prompt the user for two integers
+let num1 = parseInt(prompt("Enter the first integer:"), 10);
+let num2 = parseInt(prompt("Enter the second integer:"), 10);
+
+// Compare and display the result
+if (num1 > num2) {
+    document.write("The larger number is: " + num1);
+} else if (num2 > num1) {
+    document.write("The larger number is: " + num2);
+} else {
+    document.write("Both numbers are equal.");
+}
