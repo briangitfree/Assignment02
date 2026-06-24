4b5935255eba7ec355245edd20488d461bb38961
briangitfree <vanme98@yahoo.com>
2026-06-24 12:08:49 -0700

    odd or even is working

--- 05-odd-or-even.js
+++ 05-odd-or-even.js
@@ -2,6 +2,10 @@
 // Comp 648 JavaScript 1
 // Author: Brian Dinh
 
+
+// set the for loop for 15 times and use the modulus operation to check 
+// the remainder of the number when divided by 2 to determine if it is odd or even number
+
 for (let i = 0; i <= 15; i++) {
   if (i % 2 === 0) {
     console.log(i + " is even");
